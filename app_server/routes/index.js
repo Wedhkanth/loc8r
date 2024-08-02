247e1c39a434f7769f6f78045bb74516dfaeb67b
maniiii07 <manipreethambalmuri13@gmail.com>
2024-08-02 14:58:01 +0530

    first commit

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express wedkanth' });
+  res.render('index', { title: 'Express harsha' });
 });
 router.get('/location', function(req, res, next) {
   res.render('index', { title: 'Location info' });
